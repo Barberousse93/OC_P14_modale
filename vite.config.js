e3ae3e4a32ac775024223845d21b6e0d6c37a062
Barberousse93 <mobile.barberousse@gmail.com>
2023-09-14 13:46:42 +0200

    Configuration VITE pour publication sur npm

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path"
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/Modale/index.jsx"),
+      name: "Modale",
+      fileName: (format) => `Modale.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+        },
+      },
+    },
+  },
   plugins: [react()],
 })
